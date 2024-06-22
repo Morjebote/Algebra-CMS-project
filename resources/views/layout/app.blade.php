df46228b25a27ec5d00b3e6d692d126bde874a33
Morjebote <tomislav.mor@hotmail.com>
2024-06-22 12:17:42 +0200

    uncall @vite

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -12,9 +12,6 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-    <!-- Custom CSS -->
-    @vite('resources/css/app.css')
-
     <title>
         @yield('title')
     </title>
@@ -29,8 +26,6 @@
     <!-- Bootstrap Bundle with Popper JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
-    <!-- Custom JS -->
-    @vite('resources/js/app.js')
 </body>
 
 </html>
